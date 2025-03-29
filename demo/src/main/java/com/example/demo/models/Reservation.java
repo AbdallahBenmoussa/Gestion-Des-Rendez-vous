0d7e9e159f8b573e9548cb3bb18285ae8fc49057
Eggy <yassermhamedi333@gmail.com>
2025-03-30 00:43:19 +0100

    Final Commit 2

--- demo/src/main/java/com/example/demo/models/Reservation.java
+++ demo/src/main/java/com/example/demo/models/Reservation.java
@@ -78,9 +78,9 @@ public class Reservation {
     }
 
     private int generateId() {
-        return Long.getLong((int)salle.getBatiment()
+        return (int)Long.parseLong((int)salle.getBatiment()
                 + salle.getCodeSalle().substring(1)
                 + dateRes.toString().replaceAll("-", "")
-                + heureDebut.toString().replaceAll(":", "")).hashCode();
+                + heureDebut.toString().replaceAll(":", ""));
     }
 }
